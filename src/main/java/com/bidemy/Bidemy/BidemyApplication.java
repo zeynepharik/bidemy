ba0a562919b60cca79a5b4b5065b5eb7d66d43b2
Zeynep Harık <zeynepharik@gmail.com>
2025-04-10 14:57:58 +0300

    Initial commit for clean Bidemy project

---
+++ src/main/java/com/bidemy/Bidemy/BidemyApplication.java
@@ -0,0 +1,13 @@
+package com.bidemy.Bidemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bidemy/Bidemy/BidemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidemy.Bidemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
