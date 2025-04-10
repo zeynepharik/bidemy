9408de983d555f83045fa1d036b05285435dce8a
Zeynep Harık <zeynepharik@gmail.com>
2025-04-10 16:49:14 +0300

    Implement repository class for managing entities

---
+++ src/main/java/com/bidemy/repository/VideoRepository.java
@@ -0,0 +1,9 @@
+package com.bidemy.repository;
+
+import com.bidemy.model.entity.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VideoRepository extends JpaRepository<Video,Long> {
+}
