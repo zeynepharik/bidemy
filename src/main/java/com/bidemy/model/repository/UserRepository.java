a7fd6b74f5bdbd4abcc35babb513deecf7b5c01e
Zeynep Harık <zeynepharik@gmail.com>
2025-04-13 21:21:56 +0300

    package changed

--- src/main/java/com/bidemy/model/repository/UserRepository.java
+++ src/main/java/com/bidemy/model/repository/UserRepository.java
@@ -1,6 +1,7 @@
-package com.bidemy.repository;
+package com.bidemy.model.repository;
 
-import com.bidemy.entity.User;
+
+import com.bidemy.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
