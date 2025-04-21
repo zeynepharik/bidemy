85b947310597b4017d360ea26524d48f5b81fa83
Zeynep Harık <zeynepharik@gmail.com>
2025-04-21 14:01:45 +0300

    JWT setup completed

--- src/main/java/com/bidemy/BidemyApplication.java
+++ src/main/java/com/bidemy/BidemyApplication.java
@@ -2,14 +2,11 @@ package com.bidemy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan(basePackages ={"com.bidemy"})
-@ComponentScan(basePackages ={"com.bidemy"})
-@EnableJpaRepositories(basePackages ={"com.bidemy"})
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class BidemyApplication {
