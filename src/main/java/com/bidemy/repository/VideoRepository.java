aba49774407859f60de1be8d3eef5f21ab2a5eab
Zeynep Harık <zeynepharik@gmail.com>
2025-05-12 17:26:54 +0300

    add missing import to VideoRepository

--- src/main/java/com/bidemy/repository/VideoRepository.java
+++ src/main/java/com/bidemy/repository/VideoRepository.java
@@ -1,6 +1,7 @@
 package com.bidemy.repository;
 
 
+import com.bidemy.model.entity.VideoContent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
