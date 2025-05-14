23597dfbf16e44048a32ba2244628ba0fd92fc2e
Zeynep Harık <zeynepharik@gmail.com>
2025-05-14 23:47:08 +0300

    Added serializable to UserDTO class

--- src/main/java/com/bidemy/model/dto/UserDTO.java
+++ src/main/java/com/bidemy/model/dto/UserDTO.java
@@ -5,11 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDTO {
+public class UserDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
     private Long id;
     private String fullName;
     private String email;
