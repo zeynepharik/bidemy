0569794a13ad331b95b94a389e5ea73906f7c7ff
Zeynep Harık <zeynepharik@gmail.com>
2025-05-15 09:58:34 +0300

    updated dto layer

--- src/main/java/com/bidemy/model/dto/QuestionDTO.java
+++ src/main/java/com/bidemy/model/dto/QuestionDTO.java
@@ -1,7 +1,16 @@
 package com.bidemy.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuestionDTO {
     private Long id;
     private String text;
