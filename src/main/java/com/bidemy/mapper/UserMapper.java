02dc38ca6b855b01d05e1f8a2dc0362ba4bd646c
Zeynep Harık <zeynepharik@gmail.com>
2025-05-15 10:33:02 +0300

    updated UserMapper class

--- src/main/java/com/bidemy/mapper/UserMapper.java
+++ src/main/java/com/bidemy/mapper/UserMapper.java
@@ -2,7 +2,7 @@ package com.bidemy.mapper;
 
 import com.bidemy.model.dto.UserDTO;
 import com.bidemy.model.entity.User;
-import com.bidemy.model.entity.UserRequest;
+import com.bidemy.model.request.UserRequest;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
