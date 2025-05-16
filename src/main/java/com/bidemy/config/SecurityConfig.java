a3185a94d57cce13d2babd3ee3fe69facd3bada3
Zeynep Harık <zeynepharik@gmail.com>
2025-05-16 13:02:56 +0300

    Updated security config

--- src/main/java/com/bidemy/config/SecurityConfig.java
+++ src/main/java/com/bidemy/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
 //                        .requestMatchers(AUTHENTICATE, REGISTER, REFRESH_TOKEN,  "/login", "/css/**", "/images/**").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint(authEntryPoint)
